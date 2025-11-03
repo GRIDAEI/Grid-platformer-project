504e65c8ef1c804df32a60603d4866a36e875a33
Wojtanello <walonale@gmail.com>
2025-11-03 22:18:56 +0100

    sortowanie koordów

--- Scenes/camera.gd
+++ Scenes/camera.gd
@@ -4,8 +4,12 @@ extends Camera2D
 @export var BoundingPoint2: Vector2 = Vector2(0,0)
 var vel
 
+func _ready() -> void:
+	var temp = BoundingPoint1.min(BoundingPoint2)
+	BoundingPoint2 = BoundingPoint1.max(BoundingPoint2)
+	BoundingPoint1 = temp
+	
 func _process(delta: float) -> void:
-	print(position)
 	vel = GameManager.player.position - position
 	
 	position += vel * delta
